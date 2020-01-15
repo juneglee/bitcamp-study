b341ccd8fb77326fe38c842334501fb9c612401e
juneglee <klcpop1@gmail.com>
2020-01-16 00:30:43 +0900

    2020-01-16

--- bitcamp-project/src/main/java/com/eomcs/lms/App.java
+++ bitcamp-project/src/main/java/com/eomcs/lms/App.java
@@ -21,7 +21,34 @@ public class App {
 
   static Stack<String> commandStack = new Stack<>();
   static Queue<String> commandQueue = new Queue<>();
-
+  // static 을 선언한 이유??
+  // 정적 필드와 정적 메소드는 클래스에 고정된 멤버이므로 클래스 로더가 클래스(바이트코드)를 로딩해서 
+  // 메소드 영역에 적재할때 클래스별로 관리된다. 따라서 클래스 로딩이 끝나면 바로 사용할 수 있다 
+  // 필드를 선언할 때 인스턴스 필드로 선언할 것인가. 아니면 정적 필드로 선언할 것인가의 
+  // 판단 기준은 객체마다 가지고 있어야 할 데이터라면 인스턴스 필드로 선언하고 
+  // 객체마다 가지고 있을 필요성이 없는 공용적인 데이터라면 정적 필드로 선언하는 것이 좋다 
+  // 예를 들어  calculator클래스에서 원의 널이나 둘레를 구할 때 필요한 파이는  calculator 객체마다 
+  // 가지고 있을 필요가 없는 변핮 ㅣ안흔 ㄴ공용적인 데이터 이므로 정적 필드로 선언하는 것이 좋다 
+  // 그러나 객체별로 색깔이 다르다면 색깔은 인스턴스 필드로 선언해야 한다
+  /*
+  public class Calculator {
+	  String color; // 계산기 별로 색깔이 다를 수 있다 
+	  Static double pi = 3.14159 // 계산기에서 사용하는 파이는 값은 동일
+  }
+	*/
+  // 메서드의 경우, 인스턴스 메소드로 선언할 것인가, 아니면 정적 메소드로 선언할 것인가의 판단 기준은 
+  // 인스턴스 필드를 이용해서 실행해야 한다면 인스턴스 메소드로 선언하고, 인스턴스 필드를 이용하지 않는다면 정적 메소드로 선언한다 
+  // 예를 들어 calculator 클래스의 덧셈, 뺄샘 기능은 인스턴스 필드를 이용하기보다는 외부에서 주어진 매개값들을 가지고 덧셈과 뺄셈을 
+  // 수행하므로 정적 메소드로 선언하는 것이 좋다. 그러나 인스턴스 필드인 색깔을 변경하는 메소드는 인스턴스 메소드로 선언해야 한다 
+  /*
+  public Calculator {
+	  String color;  
+	  void setColar(String color) {this.color = color}
+	  static int plus (int x, int y) {return x+y);
+	  static int minus (int x, int y) {return x-y);
+	  }
+  }
+  */ 
   public static void main(String[] args) {
 
     Prompt prompt = new Prompt(keyboard);
