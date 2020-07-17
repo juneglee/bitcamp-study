af387e70bd8b7506cfa074832e58a846ef9d8610
juneglee <klcpop1@gmail.com>
2020-07-17 23:36:48 +0900

    [mod] net

--- bitcamp-java/src/main/java/com/eomcs/net/ex08/Exam0310.java
+++ bitcamp-java/src/main/java/com/eomcs/net/ex08/Exam0310.java
@@ -1,12 +1,12 @@
 // Apache HttpComponent 사용 - HttpGet 클래스
 package com.eomcs.net.ex08;
 
-import org.apache.http.HttpEntity;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.util.EntityUtils;
+import org.apache.hc.client5.http.classic.methods.HttpGet;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpResponse;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.core5.http.HttpEntity;
+import org.apache.hc.core5.http.io.entity.EntityUtils;
 
 public class Exam0310 {
 
